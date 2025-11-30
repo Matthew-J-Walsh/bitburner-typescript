4573a4e1fd9247472507ce2cde1234fb8d8888d4
Matthew-J-Walsh <matthewwalsh2021@u.northwestern.edu>
2025-11-30 00:48:10 -0600

    stanek script in actual

--- src/scripts/stanekScript.js
+++ src/scripts/stanekScript.js
@@ -1,4 +1,14 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    while (true) await ns.share();
+    const fragments = ns.args;
+
+    while (true) {
+        let frag = fragments[Math.floor(Math.random() * fragments.length)];
+        let point = frag.split(',');
+        for (let i = 0; i < 10; i++)
+            await ns.stanek.chargeFragment(
+                parseInt(point[0]),
+                parseInt(point[1]),
+            );
+    }
 }
